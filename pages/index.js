8c84ebba58a875efb5a74d86e44b51acd2c84f9b
Mohid Meer <mohidfauji@gmail.com>
2023-09-23 18:38:50 +0500

    Seo

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,22 @@ import AppLayout from "@/layouts/AppLayout";
 
 export default function Home() {
   return (
+    <>
+    <Head>
+                <title>{'Hostbuilding'}</title>
+                <meta name="description" content={"New Jersey Trusted Roofing Contractor. Protecting what matters the most. Whether you're in need of a full roof replacement or a roof repair, you can coun"} />
+                <meta property="og:description" content={"New Jersey Trusted Roofing Contractor. Protecting what matters the most. Whether you're in need of a full roof replacement or a roof repair, you can coun"} />
+                {/* <meta property="og:image" content={ogimgurl} />
+                 <meta property="og:image:width" content="400" />
+                    <meta property="og:image:height" content="400" /> */}
+                <meta property="og:title" content={'Hostbuilding'} />
+
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content={'Hostbuilding'} />
+                <meta name="twitter:description" content={"New Jersey Trusted Roofing Contractor. Protecting what matters the most. Whether you're in need of a full roof replacement or a roof repair, you can coun"} />
+                {/* <meta name="twitter:image" content={ogimgurl} /> */}
+            </Head>
     <AppLayout>
         <TopBar/>
         <Hero/>
@@ -24,5 +40,8 @@ export default function Home() {
         <Pricing/>
         <Footer/>
     </AppLayout>
+    
+    
+    </>
   )
 }
